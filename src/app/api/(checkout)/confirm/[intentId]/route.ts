9ea76f2e56c03d6d2612b89f561a57887753e0ac
jashanpreet kaur <jshnmeeson92136@gmail.com>
2026-05-23 19:41:31 -0600

    fix api route params for vercel build

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -3,14 +3,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const PUT = async (
   req: NextRequest,
-  { params }: { params: Promise<{ intentId: string }> },
+  { params }: { params: { intentId: string } },
 ) => {
-  const { intentId } = await params;
+  const { intentId } = params;
+
   try {
     await prisma.order.updateMany({
       where: { intent_id: intentId },
       data: { status: "being prepared" },
     });
+
     return NextResponse.json(
       { message: "Order has been updated!" },
       { status: 201 },
